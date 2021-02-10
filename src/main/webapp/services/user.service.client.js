3f1640eb25d31aaff1b01295ebebe7ffc5435014
tianyi-debug <60368126+tianyi-debug@users.noreply.github.com>
2021-02-10 15:56:27 -0500

    admin template

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,44 +1,57 @@
-function CourseServiceClient() {
-  this.createCourse = createCourse;
-  this.findAllCourses = findAllCourses;
-  this.findCourseById = findCourseById;
-  this.deleteCourse = deleteCourse;
-  this.updateCourse = updateCourse;
-  this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/courses';
-  var self = this;
-  function createCourse(course) {
-    return fetch(self.url, {
-      method: 'POST',
-      body: JSON.stringify(course),
-      headers: {
-        'content-type': 'application/json'
-      }
-    }).then(function (response) {
-      return response.json()
-    })
-  }
-  function findAllCourses() {
-    return fetch(self.url).then(function (response) {
-      return response.json()
-    })
-  }
-  function findCourseById(courseId) {
+function AdminUserServiceClient() {
 
-  }
-  function updateCourse(courseId, course) {
-    return fetch(`${self.url}/${courseId}`, {
-      method: 'PUT',
-      body: JSON.stringify(course),
-      headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => response.json())
-  }
-  function deleteCourse(courseId) {
-    // return fetch(self.url+"/"+courseId)
-    return fetch(`${self.url}/${courseId}`, {method: 'DELETE'})
-      .then(function (response) {
-        return response.json()
-      })
-  }
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/chen.tianyi/users';
+
+    var self = this;
+
+    var users = [];
+
+    function createUser(user) {
+        users.push(user);
+        return users;
+    }
+
+
+    function findAllUsers() {
+        return $.getJSON("../admin/users.json",function (data) {
+                users = data;
+                return users;
+        })
+    }
+
+
+    function findUserById(userId) {
+        for (var i = 0; i < users.length; i++) {
+            if(users[i].id==userId){
+                return users[i];
+            }
+        }
+    }
+
+    function updateUser(userId, user) {
+        for (var i = 0; i < users.length; i++) {
+            if(users[i].id==userId){
+                users[i] = user;
+                return users;
+            }
+        }
+    }
+
+
+    function deleteUser(userId) {
+        for (var i = 0; i < users.length; i++) {
+            if(users[i].id==userId){
+                users.splice(i,1);
+                break;
+            }
+        }
+        return users;
+    }
+
+
 }
